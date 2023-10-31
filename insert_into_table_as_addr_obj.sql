25428bd55c6d9428d8e45980541517c31e50801e
God <nastyasch0806@gmail.com>
2023-11-01 02:01:11 +0300

    добавлены запрос на получение информации из БД, где встречается в типе данных NAME ул

--- insert_into_table_as_addr_obj.sql
+++ insert_into_table_as_addr_obj.sql
@@ -5,8 +5,8 @@ INSERT INTO my_db.as_addr_obj VALUES (4, 'п Екуша', '1451562', CURRENT_DAT
 INSERT INTO my_db.as_addr_obj VALUES (5, 'ул.Димитрова', '1455887', '2010-12-20', DATE_ADD(current_date, interval 1 year), FALSE, FALSE, 200);
 UPDATE my_db.as_addr_obj SET my_db.as_addr_obj.start_date = '2010-12-20' WHERE my_db.as_addr_obj.object_id = 1;
 
+
 select my_db.as_addr_obj.type_name, my_db.as_addr_obj.name from my_db.as_addr_obj where type_name IN ('1422396', '1455887') and start_date='2010-12-20';
 
-
-
 SELECT * FROM my_db.as_addr_obj;
+SELECT * FROM my_db.as_addr_obj WHERE name LIKE '%ул%';
